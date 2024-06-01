458b9433a298d27e26d3e051996ec181866f4385
Sahil Khan <168674941+sahil20058@users.noreply.github.com>
2024-06-01 19:39:14 +0000

    conversion operation done

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33"=> 33
+// "33abc" => nan
+// true => 1; false => 0
+
+let isLoggedIn = "sahil"
+
+let booleanIsLoggedin = Boolean(isLoggedIn);
+console.log(booleanIsLoggedin);
+
+// 1 => true , 0=> false
+//"" => false
+//"sahil" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
