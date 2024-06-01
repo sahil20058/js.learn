b168f5baa0bcb3a9f97a4ea541e734339d9e09a8
Sahil Khan <168674941+sahil20058@users.noreply.github.com>
2024-06-01 19:00:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3)// we are using node js not browser 
+
+console.log(3+3) // code readability should be high
+console.log("sahil") 
+
+
+let name = "sahil"
+let age=19
+let isloggedin = false
+let state = null
+
+// number=> 2 to power 53
+//bigint
+//string=> ""
+//boolean => true/false
+//null => standalone value
+//undefined =>  value not assigned
+// symbol => unique
+
+//object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null); // object
